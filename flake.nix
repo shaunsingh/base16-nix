27351e8d64bf399d24afdf2e436bdecb8f2b9777
Dmitriy Kholkin <alukard.develop@gmail.com>
2021-09-28 02:21:20 +0300

    fix flakes

--- flake.nix
+++ flake.nix
@@ -2,60 +2,55 @@
   description = "Base16-template builder for nix.";
 
   inputs.nixpkgs.url = "nixpkgs/release-21.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
-    flake-utils.lib.eachDefaultSystem
-    (system:
-      with nixpkgs.legacyPackages.${system};
-      {
-        # Home-Manager Module
-        hmModule = ./base16.nix;
+  outputs = { self, nixpkgs }@inputs:
+    with nixpkgs.legacyPackages."x86_64-linux"; rec {
+      # Home-Manager Module
+      hmModule = ./base16.nix;
 
-        packages.update-base16 = (let
-          mkScript = { name, file, env ? [ ] }:
-            writeTextFile {
-              name = "${name}";
-              executable = true;
-              destination = "/bin/${name}";
-              text = ''
-                for i in ${lib.concatStringsSep " " env}; do
-                  export PATH="$i/bin:$PATH"
-                done
-                exec ${bash}/bin/bash ${file} $@
-              '';
-            };
-        in mkScript rec {
-          name = "update-base16";
-          env = [ curl nix-prefetch-git gnused jq ];
-          file = writeTextFile {
-            name = "${name}.sh";
+      packages.x86_64-linux.update-base16 = (let
+        mkScript = { name, file, env ? [ ] }:
+          writeTextFile {
+            name = "${name}";
             executable = true;
+            destination = "/bin/${name}";
             text = ''
-              generate_sources () {
-                out=$1
-                curl "https://raw.githubusercontent.com/chriskempson/base16-$out-source/master/list.yaml"\
-                | sed -nE "s~^([-_[:alnum:]]+): *(.*)~\1 \2~p"\
-                | while read name src; do
-                    echo "{\"key\":\"$name\",\"value\":"
-                    nix-prefetch-git $src
-                    echo "}"
-                  done\
-                | jq -s ".|del(.[].value.date)|from_entries"\
-                > $out.json
-              }
-              generate_sources templates &
-              generate_sources schemes &
-              wait
+              for i in ${lib.concatStringsSep " " env}; do
+                export PATH="$i/bin:$PATH"
+              done
+              exec ${bash}/bin/bash ${file} $@
             '';
           };
-        });
-
-        defaultPackage = packages.update-base16;
-
-        devShell = mkShell {
-          buildInputs = [ packages.update-base16 ];
+      in mkScript rec {
+        name = "update-base16";
+        env = [ curl nix-prefetch-git gnused jq ];
+        file = writeTextFile {
+          name = "${name}.sh";
+          executable = true;
+          text = ''
+            generate_sources () {
+              out=$1
+              curl "https://raw.githubusercontent.com/chriskempson/base16-$out-source/master/list.yaml"\
+              | sed -nE "s~^([-_[:alnum:]]+): *(.*)~\1 \2~p"\
+              | while read name src; do
+                  echo "{\"key\":\"$name\",\"value\":"
+                  nix-prefetch-git $src
+                  echo "}"
+                done\
+              | jq -s ".|del(.[].value.date)|from_entries"\
+              > $out.json
+            }
+            generate_sources templates &
+            generate_sources schemes &
+            wait
+          '';
         };
-      }
-    );
+      });
+
+      defaultPackage.x86_64-linux = packages.x86_64-linux.update-base16;
+
+      devShell.x86_64-linux = mkShell {
+        buildInputs = [ packages.x86_64-linux.update-base16 ];
+      };
+    };
 }
